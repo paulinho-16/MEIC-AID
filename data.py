2298d2c99068e3b1f1f994650625c39d1538fa55
Pedro <up201809694@fe.up.pt>
2022-11-23 18:45:47 +0000

    Generate documents

--- data.py
+++ data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import csv
 from faker import Faker
 import random
 
@@ -9,44 +10,73 @@ def with_probability(probability):
     return random.random() < probability
 
 # generate fake data and store into a JSON file
-def generate_users(records):
+def generate_users():
+    user_fields = ['id', 'name', 'address', 'date', 'phone', 'email', 'excluded', 'approved', 'balance', 'countryId']
+
     for lang in languages:
         fake = Faker(lang)
         
+        records = random.randint(75, 125)
         
         for _ in range(records):
-            user = {}
-            user['id'] = fake.random_number(digits=5)
-            user['name'] = fake.name()
-            user['address'] = fake.address().replace('\n', " ")
-            user['birth_date'] = fake.date()
-            user['phone'] = str(fake.phone_number())
-            user['email'] = str(fake.email())
-            user['excluded'] = with_probability(0.05)
-            user['approved'] = with_probability(0.9)
-            user['balance'] = fake.random_number(digits=6)
-            user['country'] = fake.current_country_code()
+            user = [fake.random_number(digits=5), fake.name(), fake.address().replace('\n', " "), fake.date(), str(fake.phone_number()), str(fake.email()), with_probability(0.08), with_probability(0.8), fake.random_number(digits=6),fake.current_country_code()]
             users.append(user)
 
+    with open('csv/user.csv', mode='w+', newline='') as f:
+        file_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        file_writer.writerow(user_fields)
+        file_writer.writerows(users)
+
+
 def generate_countries():
-    df = pd.DataFrame()
     countries = []
+    country_fields = ['id', 'name']
     
     for lang in languages:
-        country = {}
         fake = Faker(lang)
-        country['id'] = fake.current_country_code()
-        country['name'] = fake.current_country()
+        country = [fake.current_country_code(), fake.current_country()]
         countries.append(country)
 
-    dfc = pd.DataFrame(countries)
-    dfc.to_csv('country.csv', index=False)
+    # dfc = pd.DataFrame(countries)
+    # dfc.to_csv('country.csv', index=False, encoding='utf-8', sep = ';')
+
+    with open('csv/country.csv', mode='w+', newline='') as f:
+        file_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        file_writer.writerow(country_fields)
+        file_writer.writerows(countries)
+
+def generate_documents():
+    documents = []
+    fake = Faker()
+    approved = True
+    document_fields = ['id', 'type', 'file', 'approved', 'userId']
+    for user in users:
+
+        if user[7]:
+            approved = True
+        else:
+            approved = False
+
+        ext = random.choice(['pdf', 'png', 'jpeg'])
+
+        document = [fake.random_number(digits=5), random.choice(['Driver License', 'Identification Card', 'Bill']), fake.file_name(extension=ext), approved, user[0]]
+        documents.append(document)
+        # sera necessario adicionar id do doc ao user?
+
+    # dfd = pd.DataFrame(documents)
+    # dfd.to_csv('document.csv', index=False, encoding='utf-8', sep = ';')
+
+    with open('csv/document.csv', mode='w+', newline='') as f:
+        file_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        file_writer.writerow(document_fields)
+        file_writer.writerows(documents)
 
 
 if __name__ == "__main__":
-    generate_users(100)
-
+    generate_users()
     generate_countries()
+    generate_documents()
 
-    df = pd.DataFrame(users)
-    df.to_csv('user.csv', index=False)
+    # df = pd.DataFrame(users)
+    # df.to_csv('user.csv', index=False, encoding='utf-8', sep = ';')
+    
