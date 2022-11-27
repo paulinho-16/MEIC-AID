a29d37e3241b942f03f711ed57fd16f4d9946509
Paulo Ribeiro <pjsalgadomribeiro@gmail.com>
2022-11-27 21:04:00 +0000

    Creating truncated dataset

--- data.py
+++ data.py
@@ -1,7 +1,11 @@
+import os
 import pandas as pd
 import csv
 from faker import Faker
 import random
+
+DATA_DIR = './data'
+NUM_ROWS_PER_SPORT = 100000
 
 users = []
 languages = ['pt_PT', 'es_ES', 'fr_FR', 'en_US']
@@ -22,7 +26,8 @@ def generate_users():
             user = [fake.random_number(digits=5), fake.name(), fake.address().replace('\n', " "), fake.date(), str(fake.phone_number()), str(fake.email()), with_probability(0.08), with_probability(0.8), fake.random_number(digits=6),fake.current_country_code()]
             users.append(user)
 
-    with open('csv/user.csv', mode='w+', newline='') as f:
+    filename = os.path.join(DATA_DIR, 'user.csv')
+    with open(filename, mode='w+', newline='') as f:
         file_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         file_writer.writerow(user_fields)
         file_writer.writerows(users)
@@ -37,7 +42,8 @@ def generate_countries():
         country = [fake.current_country_code(), fake.current_country()]
         countries.append(country)
 
-    with open('csv/country.csv', mode='w+', newline='') as f:
+    filename = os.path.join(DATA_DIR, 'country.csv')
+    with open(filename, mode='w+', newline='') as f:
         file_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         file_writer.writerow(country_fields)
         file_writer.writerows(countries)
@@ -60,13 +66,29 @@ def generate_documents():
         documents.append(document)
         # sera necessario adicionar id do doc ao user?
 
-    with open('csv/document.csv', mode='w+', newline='') as f:
+    filename = os.path.join(DATA_DIR, 'document.csv')
+    with open(filename, mode='w+', newline='') as f:
         file_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         file_writer.writerow(document_fields)
         file_writer.writerows(documents)
 
+def generate_bets():
+    df = pd.read_csv('./data/betfair_140901.csv')
+
+    df_portuguese_soccer = df.loc[df['FULL_DESCRIPTION'].str.contains('Portuguese Soccer', na=False)]
+    df_portuguese_soccer = df_portuguese_soccer.replace('Sp Lisbon', 'Sporting Clube de Portugal', regex=True) # fixing Betfair's ignorance
+    df_other_soccer = df.loc[df['SPORTS_ID'] == 1 & ~df['FULL_DESCRIPTION'].str.contains('Portuguese Soccer', na=False)].head(NUM_ROWS_PER_SPORT - df_portuguese_soccer.shape[0])
+    df_soccer = pd.concat([df_portuguese_soccer, df_other_soccer], ignore_index=True, sort=False)
+    df_tennis = df.loc[df['SPORTS_ID'] == 2].head(NUM_ROWS_PER_SPORT)
+    df_basket = df.loc[df['SPORTS_ID'] == 7522].head(NUM_ROWS_PER_SPORT)
+
+    df_truncated = pd.concat([df_soccer, df_tennis, df_basket], ignore_index=True, sort=False)
+
+    filename = os.path.join(DATA_DIR, 'betfair.csv')
+    df_truncated.to_csv(filename, index=False, encoding='utf-8', sep=';')
 
 if __name__ == "__main__":
     generate_users()
     generate_countries()
     generate_documents()
+    generate_bets()
