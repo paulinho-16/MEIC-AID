8f1f429e9fa6131edab1aa14a9884153bbf3c045
Pedro <up201809694@fe.up.pt>
2022-12-03 14:47:28 +0000

    More locations for users

--- data.py
+++ data.py
@@ -8,26 +8,28 @@ DATA_DIR = './data'
 NUM_ROWS_PER_SPORT = 100000
 
 users = []
-languages = ['pt_PT', 'es_ES', 'fr_FR', 'en_US']
+languages = [('pt_PT', 150), ('es_ES', 115), ('fr_FR', 100), ('en_GB', 75), ('it_IT', 125), ('nl_NL', 69), ('de_DE', 95), ('da_DK', 86), ('sk_SK', 35), ('sv_SE', 50)]
 
 def with_probability(probability):
     return random.random() < probability
 
-# generate fake data and store into a JSON file
+# generate fake data and store into a CSV file
 def generate_users():
     user_fields = ['id', 'name', 'address', 'date', 'phone', 'email', 'excluded', 'approved', 'balance', 'countryId']
+    id = 1
 
     for lang in languages:
-        fake = Faker(lang)
+        fake = Faker(lang[0])
         
-        records = random.randint(75, 125)
+        # records = random.randint(75, 125)
         
-        for _ in range(records):
-            user = [fake.random_number(digits=5), fake.name(), fake.address().replace('\n', " "), fake.date(), str(fake.phone_number()), str(fake.email()), with_probability(0.08), with_probability(0.8), fake.random_number(digits=6),fake.current_country_code()]
+        for _ in range(lang[1]):
+            user = [id, fake.name(), fake.address().replace('\n', " "), fake.date_between(start_date='-90y', end_date='-18y'), str(fake.phone_number()), str(fake.email()), with_probability(0.08), with_probability(0.8), fake.random_number(digits=6),fake.current_country_code()]
             users.append(user)
+            id += 1
 
     filename = os.path.join(DATA_DIR, 'user.csv')
-    with open(filename, mode='w+', newline='') as f:
+    with open(filename, mode='w+', newline='', encoding='utf-8') as f:
         file_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         file_writer.writerow(user_fields)
         file_writer.writerows(users)
@@ -38,12 +40,12 @@ def generate_countries():
     country_fields = ['id', 'name']
     
     for lang in languages:
-        fake = Faker(lang)
+        fake = Faker(lang[0])
         country = [fake.current_country_code(), fake.current_country()]
         countries.append(country)
 
     filename = os.path.join(DATA_DIR, 'country.csv')
-    with open(filename, mode='w+', newline='') as f:
+    with open(filename, mode='w+', newline='', encoding='utf-8') as f:
         file_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         file_writer.writerow(country_fields)
         file_writer.writerows(countries)
@@ -67,7 +69,7 @@ def generate_documents():
         # sera necessario adicionar id do doc ao user?
 
     filename = os.path.join(DATA_DIR, 'document.csv')
-    with open(filename, mode='w+', newline='') as f:
+    with open(filename, mode='w+', newline='', encoding='utf-8') as f:
         file_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         file_writer.writerow(document_fields)
         file_writer.writerows(documents)
@@ -117,9 +119,14 @@ def generate_bets():
         filename = os.path.join(DATA_DIR, f'{table}.csv')
         df.to_csv(filename, index=False, encoding='utf-8', sep=',')
 
-if __name__ == "__main__":
+def main():
     generate_users()
     generate_countries()
     generate_documents()
     truncate_dataset()
     generate_bets()
+
+if __name__ == "__main__":
+    main()
+    
+    
